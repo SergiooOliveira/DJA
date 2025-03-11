bce98dedb579e379364eec5e7b78eb014fa1b470
SergiooOliveira <58986379+SergiooOliveira@users.noreply.github.com>
2025-03-11 18:33:42 +0000

    Start Menu and transitions ready

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("RoomTest");
+    }
+
+    public void CallOptionsMenu()
+    {
+        SceneManager.LoadScene("OptionsMenu");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
